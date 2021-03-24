dc40d5e6ba352d30b8c96c7bfe6309cbd72a5430
疯狂的石头 <>
2021-03-24 18:35:12 +0800

    点击事件

--- MassageChair/PanelViewController.m
+++ MassageChair/PanelViewController.m
@@ -21,12 +21,27 @@
 @property(nonatomic,weak)IBOutlet UISwitch * voiceSwitch;
 @property(nonatomic,weak)IBOutlet UIView * progressBackgroundView;
 @property(nonatomic,weak)IBOutlet UIView * leftView;
+@property(nonatomic,weak)IBOutlet NSLayoutConstraint * leftViewLeftConstraint;
+@property(nonatomic,weak)IBOutlet NSLayoutConstraint * leftViewTopConstraint;
+
 @property(nonatomic,weak)IBOutlet UIView * rightView;
+@property(nonatomic,weak)IBOutlet NSLayoutConstraint * rightViewRightConstraint;
+@property(nonatomic,weak)IBOutlet NSLayoutConstraint * rightViewTopConstraint;
 @property(nonatomic,weak)IBOutlet UIButton * timingButton;
 @property(nonatomic,weak)IBOutlet UIButton * endButton;
 @property(nonatomic,strong)ArcProgressView * arcProgressView;
 @property(nonatomic,strong)NSTimer * timer;
 @property(nonatomic,assign)NSInteger numberCount;
+
+@property(nonatomic,weak)IBOutlet UILabel * directionLabel;
+@property(nonatomic,weak)IBOutlet UILabel * speedLabel;
+@property(nonatomic,weak)IBOutlet UILabel * temperatureLabel;
+@property(nonatomic,weak)IBOutlet UILabel * modelLabel;
+
+@property(nonatomic,assign)NSInteger directionFlag;
+@property(nonatomic,assign)NSInteger speedFlag;
+@property(nonatomic,assign)NSInteger temperatureFlag;
+@property(nonatomic,assign)NSInteger modelFlag;
 
 @end
 
@@ -53,13 +68,20 @@
     self.numberCount = TIMECOUNT;
     self.arcProgressView.time = @"10:00";
     
+    CGFloat radius = self.arcProgressView.bounds.size.width/2;
     self.leftView.transform = CGAffineTransformMakeRotation(-M_PI/4);
+    self.leftViewLeftConstraint.constant = radius - radius/sqrtf(2)-6;
+    self.leftViewTopConstraint.constant = radius - radius/sqrtf(2)-18;
     self.rightView.transform = CGAffineTransformMakeRotation(M_PI/4);
+    self.rightViewRightConstraint.constant = radius - radius/sqrtf(2)-6;
+    self.rightViewTopConstraint.constant = radius - radius/sqrtf(2)-18;
     
     self.timingButton.layer.borderColor = UIColor.whiteColor.CGColor;
     self.timingButton.layer.borderWidth = 1;
     self.endButton.layer.borderColor = UIColor.whiteColor.CGColor;
     self.endButton.layer.borderWidth = 1;
+    
+    self.directionFlag = 0;
     
 }
 
@@ -79,7 +101,99 @@
     
     CGFloat progress = (CGFloat)1/TIMECOUNT;
     self.arcProgressView.progress += progress;
-    self.arcProgressView.time = [NSString stringWithFormat:@"%ld",self.numberCount];
+//    self.arcProgressView.time = [NSString stringWithFormat:@"%ld",self.numberCount];
+}
+
+/// 方向切换
+/// @param sender 按钮
+- (IBAction)directionClickAction:(UIButton *)sender{
+    NSString * directionString = nil;
+    switch (self.directionFlag) {
+        case 0:
+            directionString = @"反向";
+            self.directionFlag = 1;
+            break;
+        case 1:
+            directionString = @"交替";
+            self.directionFlag = 2;
+            break;
+        case 2:
+            directionString = @"正向";
+            self.directionFlag = 0;
+            break;
+        default:
+            break;
+    }
+    self.directionLabel.text = directionString;
+}
+
+/// 速度切换
+/// @param sender 按钮
+- (IBAction)speedClickAction:(UIButton *)sender{
+    NSString * speedString = nil;
+    switch (self.speedFlag) {
+        case 0:
+            speedString = @"中速";
+            self.speedFlag = 1;
+            break;
+        case 1:
+            speedString = @"快速";
+            self.speedFlag = 2;
+            break;
+        case 2:
+            speedString = @"慢速";
+            self.speedFlag = 0;
+            break;
+        default:
+            break;
+    }
+    self.speedLabel.text = speedString;
+}
+
+/// 温度切换
+/// @param sender 按钮
+- (IBAction)temperatureClickAction:(UIButton *)sender{
+    NSString * temperatureString = nil;
+    switch (self.temperatureFlag) {
+        case 0:
+            temperatureString = @"中速";
+            self.temperatureFlag = 1;
+            break;
+        case 1:
+            temperatureString = @"快速";
+            self.temperatureFlag = 2;
+            break;
+        case 2:
+            temperatureString = @"慢速";
+            self.temperatureFlag = 0;
+            break;
+        default:
+            break;
+    }
+    self.temperatureLabel.text = temperatureString;
+}
+
+/// 模式切换
+/// @param sender 按钮
+- (IBAction)modelClickAction:(UIButton *)sender{
+    NSString * modelString = nil;
+    switch (self.modelFlag) {
+        case 0:
+            modelString = @"中速";
+            self.modelFlag = 1;
+            break;
+        case 1:
+            modelString = @"快速";
+            self.modelFlag = 2;
+            break;
+        case 2:
+            modelString = @"慢速";
+            self.modelFlag = 0;
+            break;
+        default:
+            break;
+    }
+    self.modelLabel.text = modelString;
 }
 
 - (IBAction)endCountDown:(id)sender{
--- MassageChair/ViewController.m
+++ MassageChair/ViewController.m
@@ -7,6 +7,7 @@
 
 #import "ViewController.h"
 #import <MassageChairSDK/PanelViewController.h>
+#import <CommonCrypto/CommonCrypto.h>
 
 @interface ViewController ()
 
@@ -24,24 +25,34 @@
     [btn addTarget:self action:@selector(action) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
     
+    NSString * appId = @"a0d9ba8e8a424431b2cd36d63bbe8955";
+    NSString * bizId = @"1101999999";
+    NSString * timestamps = @"1616581839725";
+    NSString * body = [NSString stringWithFormat:@"{\"%@\":\"%@\"}",@"data",@"MgMlbWS8n4dHLxHBT4f8K4lp6/5Pw/cPBrmd1ACROHDqTpiFhJR+3lnaOZgAWrfxAchKnGD+2NpGXmYUAW/WfM4rhHb284MAhaxeqkMpgTrdDmiB3O7a4obRjA=="];
     
+    
+    NSString * string = [NSString stringWithFormat:@"appId%@bizId%@timestamps%@%@",appId,bizId,timestamps,body];
+    NSLog(@"string=%@",string);
+    
+    NSString * sign = [self sha256HashFor:string];
+    NSLog(@"sign=%@",sign);
     
 }
 
 //SHA256加密
-//+ (NSString*)sha256HashFor:(NSString*)input{
-//    const char* str = [input UTF8String];
-//    unsigned char result[CC_SHA256_DIGEST_LENGTH];
-//    CC_SHA256(str, (CC_LONG)strlen(str), result);
-//
-//    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH*2];
-//    for(int i = 0; i<CC_SHA256_DIGEST_LENGTH; i++)
-//    {
-//        [ret appendFormat:@"%02x",result[i]];
-//    }
-//    ret = (NSMutableString *)[ret uppercaseString];
-//    return ret;
-//}
+- (NSString*)sha256HashFor:(NSString*)input{
+    const char* str = [input UTF8String];
+    unsigned char result[CC_SHA256_DIGEST_LENGTH];
+    CC_SHA256(str, (CC_LONG)strlen(str), result);
+
+    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH*2];
+    for(int i = 0; i<CC_SHA256_DIGEST_LENGTH; i++)
+    {
+        [ret appendFormat:@"%02x",result[i]];
+    }
+    ret = (NSMutableString *)[ret uppercaseString];
+    return ret;
+}
 
 
 - (void)action{
